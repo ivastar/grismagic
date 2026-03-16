9f70417ae502222db5724a3786f815861b76af7b
Iva Momcheva <ivastar@users.noreply.github.com>
2026-03-16 17:15:44 +0100

    Fix test CI error

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,20 +1,9 @@
 import pytest
+import numpy as np
 
-from my_package.example import add_one
-
-
-def test_type():
-    """
-    Test addition with float, int and strung.
-    """
-    assert add_one(1) == 2
-    assert add_one(1.1) == 2.1
-    assert add_one('1') == '11'
+import grismagic
 
 
-def test_error():
-    """
-    Test addition with list.
-    """
-    with pytest.raises(TypeError):
-        add_one([1])
+def test_version():
+    assert hasattr(grismagic, "__version__")
+
